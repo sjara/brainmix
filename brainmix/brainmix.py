5cd47e1465f0f6a8e80c27b855c145c0c36a4031
Santiago Jaramillo <sjara@uoregon.edu>
2015-04-10 17:50:52 -0700

    Function BrainMix removed. It also now uses argparse instead of deprecated optparse.

--- brainmix/brainmix.py
+++ brainmix/brainmix.py
@@ -2,14 +2,15 @@
 
 '''
 BrainMix 
-Written by Kristi Potter
-March 11, 2015
+Written by Kristi Potter (2015-03-11)
+Modified by Santiago Jaramillo (2015-04-10)
 '''
 
 import sys
 import os
 import signal  # To enable Ctrl-C to quit application from terminal
-from optparse import OptionParser
+#import optparse # DEPRECATED
+import argparse
 from PySide import QtGui
 sys.path.append("./gui/")
 #sys.path.append("./Modules/")
@@ -19,14 +20,18 @@ sys.path.append("./core/")
 
 from gui import mainwindow
 
+"""
 # - - - BrainMix - - - 
 def BrainMix():
     '''
+    DEPRECATED: running the app on the global workspace enables more convenient
+                introspection of variables and graphical objects during
+                interactive sessions. -- Santiago 2015-04-10
     Main function for BrainMix.
     Function determines if user wants command line or GUI interaction.
     '''    
     # Command Line Parser business:
-    parser = OptionParser()
+    parser = optparse.OptionParser()
     parser.add_option("-c", "--command", dest="commandLine",
                       action="store_true", default=False,
                       help="Command Line Interface")
@@ -43,6 +48,20 @@ def BrainMix():
         mainWindow.show()
         #sys.exit(app.exec_())
         app.exec_()
+"""
 
 if __name__ == '__main__':
-    BrainMix()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--nogui', action='store_true',
+                        help='Run application with no User Graphical Interface.')
+    args = parser.parse_args()
+
+    if args.nogui:
+        print '[brainmix.py] This will run the application with no GUI.'
+    else:
+        signal.signal(signal.SIGINT, signal.SIG_DFL) # Enable Ctrl-C
+        app = QtGui.QApplication(sys.argv)
+        mainWindow = mainwindow.MainWindow()
+        mainWindow.show()
+        sys.exit(app.exec_())
+
