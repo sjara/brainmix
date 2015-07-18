3a7cd6c6a244e827ea8edd87fe68b296ba4e0d60
Santiago Jaramillo <sjara@uoregon.edu>
2015-07-17 19:15:24 -0700

    Removed residuals function (not necessary anymore).

--- brainmix/modules/imregistration.py
+++ brainmix/modules/imregistration.py
@@ -37,40 +37,27 @@ def rigid_body_transform(image, tfrm):
     return outimg
 
 
-def residualsOLD(tfrm, target, source):
+def rigid_body_least_squares(source, target, tfrm, maxIterations):
     '''
-    Compute pixel intensity difference between source and transformed target image.
+    Apply modified Levenberg-Marquardt algorithm to minimize the difference in pixel
+    intensities between the source and target images.
 
     Args:
-        tfrm (np.ndarray): (3,) transformation [rotation_angle, translation_x, translation_y]
-        target (np.ndarray): target (grayscale) image.
-        target (np.ndarray): target (grayscale) image.
+        source (np.ndarray): source image, the one that will be transformed.
+        target (np.ndarray): target image, the one that will not move.
+        tfrm (np.ndarray): (3,) initial transformation [rotation_angle, translation_x, translation_y].
+
     Returns:
-        err (np.ndarray): intensity difference at each pixel.
+        tfrm (np.ndarray): (3,) best transformation [rotation_angle, translation_x, translation_y].
     '''
-    targettfrm = rigid_body_transform(target, tfrm)
-    err = targettfrm-source
-    return err
-
-
-def rigid_body_least_squares(tfrm, target, sgrad, source, maxIterations):
-    '''
-    Apply modified Levenberg-Marquardt algorithm to minimise the difference in pixel
-    intensities between the source and target image samples.
-
-    Args:
-        tfrm (np.ndarray): initial tranformation
-        tspline (): spline approximation of target image.
-        sgrad (np.ndarray): (imHeight,imWidth) gradient of source image 
-        
-    Returns:
-
-    '''
-    imshape = sgrad.shape
+    imshape = source.shape
     (height, width) = imshape
-    attempt = tfrm.copy()
+    newtfrm = tfrm.copy()
     lambdavar = 1.0
-    #err = residuals(-tfrm, target, source)
+    # -- Use Scharr operator to calculate image gradient in horizontal and vertical directions --
+    scharr = np.array([[-3-3j, 0-10j, +3-3j], [-10+0j, 0+0j, +10+0j], [-3+3j, 0+10j, +3+3j]])
+    sgrad = scipy.signal.convolve2d(source, scharr, boundary='symm', mode='same')
+    # -- Calculate current error --
     err = rigid_body_transform(target, -tfrm) - source
     bestMeanSquares = np.mean(err**2)
     # -- Pre-calculate items for the Hessian (for efficiency) --
@@ -82,7 +69,6 @@ def rigid_body_least_squares(tfrm, target, sgrad, source, maxIterations):
     gradRealGradImag = sgrad.real*sgrad.imag
     gradImagSq = sgrad.imag**2
     (heightSq,widthSq) = np.array(imshape)**2
-    iterations = 1
     displacement = 1.0
     # NOTE: using range() for compatibility with Python3
     for iteration in range(maxIterations):
@@ -95,22 +81,20 @@ def rigid_body_least_squares(tfrm, target, sgrad, source, maxIterations):
                              np.sum(err*sgrad.imag)])
         sHessianDiag = np.diag(lambdavar*np.diag(sHessian))
         update = np.dot(np.linalg.inv(sHessian+sHessianDiag),gradient)
-        attempt = tfrm - update
+        attempt = newtfrm - update
         displacement = np.sqrt(update[1]*update[1] + update[2]*update[2]) + \
                        0.25 * np.sqrt(widthSq + heightSq) * np.absolute(update[0])
-        iterations += 1
-        #err = residuals(-attempt, target, source)
         err = rigid_body_transform(target, -attempt) - source
         if np.mean(err**2)<bestMeanSquares:
             bestMeanSquares = np.mean(err**2)
             # NOTE: Numpy 1.7 or newer has np.copyto() which should be faster than copy()
-            tfrm = attempt.copy() # We need to copy values, tfrm=attempt would just make a reference to 'attempt'
+            newtfrm = attempt.copy() # We need to copy values, tfrm=attempt would just make a reference to 'attempt'
             lambdavar /= 10.0 # FIXME: we may need to prevent lambda from becoming 0
         else:
             lambdavar *= 10.0
         if displacement < 0.001:
             break
-    return tfrm
+    return newtfrm
             
 
 def rigid_body_registration(source, target, pyramidDepth, minLevel=0, downscale=2, debug=False):
@@ -119,12 +103,6 @@ def rigid_body_registration(source, target, pyramidDepth, minLevel=0, downscale=
 
     This function computes the image pyramid for source and target and calculates the transformation
     that minimizes the least-square error between images (starting at the lowest resolution).
-
-    The pyramids consist of downsampled versions of the source and target image, from which the B-spline
-    coefficients are computed for the target, and the gradients are computed for the source.
-    Note that sometimes it takes a very long time to compute the transformation for the topmost
-    layer (containing the full size image) without improving much the registration, so you may want
-    to use the parameters below to limit this.
 
     Args:
         source (np.ndarray): source image, the one that will be transformed.
@@ -137,20 +115,18 @@ def rigid_body_registration(source, target, pyramidDepth, minLevel=0, downscale=
     '''
     sourcePyramid = tuple(skimage.transform.pyramid_gaussian(source, max_layer=pyramidDepth, downscale=downscale))
     targetPyramid = tuple(skimage.transform.pyramid_gaussian(target, max_layer=pyramidDepth, downscale=downscale))
-    # -- Use Scharr operator to calculate image gradient in horizontal and vertical directions --
-    scharr = np.array([[-3-3j, 0-10j, +3-3j], [-10+0j, 0+0j, +10+0j], [-3+3j, 0+10j, +3+3j]])
     tfrm = np.zeros(3)
 
     for layer in range(pyramidDepth, minLevel-1, -1):
-        sourceGradient = scipy.signal.convolve2d(sourcePyramid[layer], scharr, boundary='symm', mode='same')
-        imshape = targetPyramid[layer].shape
         tfrm[1:] *= downscale  # Scale translation for next level in pyramid
-        tfrm = rigid_body_least_squares(tfrm, targetPyramid[layer], sourceGradient, sourcePyramid[layer], 10*2**(layer-1))
+        tfrm = rigid_body_least_squares(sourcePyramid[layer],targetPyramid[layer],
+                                        tfrm, 10*2**(layer-1))
         toptfrm = np.concatenate(([tfrm[0]],tfrm[1:]*pow(downscale,layer)));
         if debug:
-            print 'Layer {0}: {1}x{2}'.format(layer,*imshape)
+            print 'Layer {0}: {1}x{2}'.format(layer, *targetPyramid[layer].shape)
             print 'th={0:0.4}, x={1:0.1f} , y={2:0.1f}'.format(*toptfrm) ### DEBUG
     return toptfrm
+
 
 
 if __name__=='__main__':
